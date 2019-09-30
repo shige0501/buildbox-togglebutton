611d3aa142f2d4fe7580339a1eb8e91ad7f0634c
Koji Shigemura <shige0501@gmail.com>
2019-09-30 22:53:54 +0900

    ToggleButtonの実装追加

--- app/src/main/java/net/buildbox/sample/buildbox_togglebutton/MainActivity.kt
+++ app/src/main/java/net/buildbox/sample/buildbox_togglebutton/MainActivity.kt
@@ -2,11 +2,18 @@ package net.buildbox.sample.buildbox_togglebutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import android.widget.ToggleButton
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sampleToggle: ToggleButton = findViewById(R.id.sample_toggle)
+        sampleToggle.setOnCheckedChangeListener { _, _ ->
+            Toast.makeText(this, "ToggleButtonの状態が変わりました", Toast.LENGTH_SHORT).show()
+        }
     }
 }
