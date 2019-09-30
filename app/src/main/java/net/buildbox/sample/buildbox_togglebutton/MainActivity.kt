cc4261e8e88ae97a3b46112a2c148a7b0a2befb8
Koji Shigemura <shige0501@gmail.com>
2019-09-30 22:36:38 +0900

    initial commit

---
+++ app/src/main/java/net/buildbox/sample/buildbox_togglebutton/MainActivity.kt
@@ -0,0 +1,12 @@
+package net.buildbox.sample.buildbox_togglebutton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
